a3ad047472130f712e54d1c136c4cbe13daaaf15
Anoop Jose <anoop@flutterly.co.uk>
2024-09-20 20:34:59 +0100

    Added latest projects section to the home page

--- src/pages/Home-Page.tsx
+++ src/pages/Home-Page.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroSection from '../components/Hero-Section';
 import Skills from '../components/skills';
+import LatestProjects from '../components/latest-projects';
 
 const HomePage: React.FC = () => {
     return (
         <div>
             <HeroSection />
             <Skills />
+            <LatestProjects />
         </div>
     );
 }
